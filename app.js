2d59cb03b18cf7c0b5c9a014adcbfe6572cb69b4
JChan91 <fourbin1221@gmail.com>
2020-11-12 16:04:27 +0900

    fix express middleware

--- app.js
+++ app.js
@@ -4,27 +4,28 @@ const port = 8000;
 const session = require('express-session');
 const cors = require('cors');
 
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 // router
 const indexRouter = require('./routes/index');
 const searchRouter = require('./routes/search');
 
 // 미들웨어 사용
-app.use(cors());
-app.use(express.json());
-// app.use(session({
-//     secret: 'keyboard cat',
-//     resave: false,
-//     saveUninitialized: true,
-//     cookie: { secure: true }
-//   }))
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true
+}))
 
+app.use('/', indexRouter);
+app.use('/searchinfo', searchRouter);
 
 // get, post
 app.get('/', (req, res) => {
     res.status(200).send('Hello World');
 })
-app.use('/', indexRouter);
-app.use('/searchinfo', searchRouter);
 
 // 서버 실행
 app.listen(port, () => {
