e0016c4005d7dc460be9414d359ba34b9345da45
JChan91 <fourbin1221@gmail.com>
2020-11-12 16:08:37 +0900

    feature signup, signin, remove

--- controller/user/signin.js
+++ controller/user/signin.js
@@ -1,9 +1,22 @@
 const { User } = require('../../models');
-const session = require('express-session');
 
+// 로그인
 module.exports = {
-    post: (req, res) => {
-        console.log(req.body);
-        res.sendStatus(500);
+  post: async (req, res) => {
+    const { email, password } = req.body;
+    let sess = req.session;
+    let findUser = await User.findOne({ where: { email, password } });
+
+    try {
+      if (findUser === null) {
+        res.status(404).send('유저를 찾을 수 없습니다.');
+      } else {
+        // 쿠키 전달
+        sess.userid = findUser.id;
+        res.status(200).json({ id: sess.userid });
+      }
+    } catch (err) {
+      res.status(500).send(err);
     }
+  }
 };
