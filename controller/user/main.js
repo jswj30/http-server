c538a794111f2dc302170ca533fcb02069a0feae
JChan91 <fourbin1221@gmail.com>
2020-11-14 11:31:35 +0900

    fix main/post

--- controller/user/main.js
+++ controller/user/main.js
@@ -1,12 +1,12 @@
-const { Todo, User, Complete, JoinTable } = require('../../models');
+const { Todo, User, Complete } = require('../../models');
+const session = require('express-session');
 
 module.exports = {
   get: async (req, res) => {
     let result = [];
-    // 받아올 데이터는 Todo.content, Complete.complete, Complete.important
     let todoList = await Todo.findAll({
       where: { userId: 1 },
-      attributes: ['content'],
+      attributes: ['content', 'startDate'],
       include: [
         {
           model: User,
@@ -20,31 +20,48 @@ module.exports = {
       ]
     });
 
-    //get: (req, res) => {
-    // let userid = 2;
-    // if (userid) {
-    //   Todo.findAll({
-    //     where: { userId: userid },
-    //     attributes: ['id', 'startDate', 'content'],
-    //     include: [
-    //    {
-    //       model: User,
-    //       attributes: ['name'],
-    //       where: { id: userid }
-    //     }, {
-    //       model: Complete,
-    //       attributes: ['important', 'complete']
-    //     }]
-    //   })r
+    for (let i = 0; i < todoList.length; i++) {
+      result.push({
+        id: 1, // 추후에 req.session.userid 변경
+        name: todoList[i].dataValues.User.dataValues.name,
+        content: todoList[i].dataValues.content,
+        startDate: todoList[i].dataValues.startDate,
+        important: todoList[i].dataValues.Completes[0].dataValues.important,
+        complete: todoList[i].dataValues.Completes[0].dataValues.complete,
+      });
+    }
 
-    console.log(todoList);
-    // for (let i = 0; i < todoList[0].dataValues.Todos.length; i++) {
-    //   console.log(todoList[0].dataValues.Todos[i].dataValues);
-    // }
-    //console.log(result);
+    try {
+      if (!result.length) {
+        res.status(404).json('아직도 시간보낼게 없어?');
+      } else {
+        res.status(200).json(result);
+      }
+    } catch (err) {
+      res.sendStatus(500);
+    }
   },
 
   post: async (req, res) => {
+    // 조인하지 않고 각 테이블에 findOrCreate
+    let { userId, content, startDate, important, complete, deleteId } = req.body;
 
-  }
+    let todo = await Todo.findOrCreate({
+      where: { userId, content, startDate },
+      default: { userId, content, startDate }
+    });
+    let com = await Complete.findOrCreate({
+      where: { important, complete, deleteId },
+      default: { important, complete, deleteId }
+    });
+
+    try {
+      if (todo && com) {
+        console.log(todo);
+        console.log(com);
+      }
+    } catch (err) {
+      res.sendStatus(500);
+    }
+  },
 }
