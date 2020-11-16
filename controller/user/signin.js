da4c6d33e19c7a8ee9f3f56b0985e7d1c4bd0f33
JChan91 <fourbin1221@gmail.com>
2020-11-17 08:02:29 +0900

    feature oauth github

--- controller/user/signin.js
+++ controller/user/signin.js
@@ -3,9 +3,30 @@ const axios = require('axios');
 
 require('dotenv').config();
 
-const github = `https://github.com/login/oauth`;
 const clientId = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
+
+// 소셜 로그인 요청 기능
+let getToken = async function (code) {
+  let res = await axios({
+    method: 'POST',
+    url: `https://github.com/login/oauth/access_token?client_id=${clientId}&client_secret=${clientSecret}&code=${code}`,
+    headers: { accept: 'application/json' }
+  });
+  console.log('요기는 토큰받는곳', res.data.access_token);
+  const access_token = res.data.access_token;
+  return access_token;
+};
+
+let getUserInfo = async function (token) {
+  let data = await axios({
+    method: 'GET',
+    url: 'https://api.github.com/user',
+    headers: { Authorization: 'token ' + token }
+  });
+
+  return data.data;
+}
 
 // 로그인
 module.exports = {
@@ -27,14 +48,37 @@ module.exports = {
     }
   },
 
-  get: async (req, res) => {
-    axios({
-      method: 'post',
-      url: `${github}/access_token?client_id=${clientID}&client_secret=${clientSecret}&code=${requestToken}`,
-      header: { accept: 'application/json' }
-    }).then((res) => {
-      const token = res.data.access_token;
-      res.redirect(`/main?access_token=${token}`);
-    })
+  github: async (req, res) => {
+    const url = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=http://localhost:8000/githubLogin`;
+    res.redirect(url);
+  },
+
+  githubLogin: async (req, res) => {
+    const code = req.query.code;
+    const token = await getToken(code);
+    const userInfo = await getUserInfo(token);
+
+    //userInfo 바탕으로 DB에 정보 저장
+    const { login } = userInfo;
+    try {
+      const userData = await User.findOrCreate({
+        where: { name: login },
+        default: { name: login }
+      });
+
+      if (userData) {
+        const [user, created] = userData;
+
+        if (created) {
+          let sess = req.session;
+          sess.userid = findUser.id;
+          res.redirect('/main');
+        } else {
+          res.redirect('/main');
+        }
+      }
+    } catch (err) {
+      res.redirect('/main');
+    }
   }
 };
