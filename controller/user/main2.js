9f6fcbc84b5ee6ce776819c3b46c7bda543287d4
Ubuntu <ubuntu@ip-172-31-43-241.ap-northeast-2.compute.internal>
2020-11-18 02:40:34 +0000

    main2

--- controller/user/main2.js
+++ controller/user/main2.js
@@ -12,7 +12,7 @@ module.exports = {
       include: [
         {
           model: User,
-          attributes: ['name']
+          attributes: ['name', 'email']
         },
         {
           model: Complete,
@@ -24,8 +24,9 @@ module.exports = {
     let result = [];
     for (let i = 0; i < todoList.length; i++) {
       result.push({
-        id: todoList[i].dataValues.userId,
+        id: todoList[i].dataValues.userId, 
         name: todoList[i].dataValues.User.dataValues.name,
+	email: todoList[i].dataValues.User.dataValues.email, 
         content: todoList[i].dataValues.content,
         startDate: todoList[i].dataValues.startDate,
         important: todoList[i].dataValues.Completes[0].dataValues.important,
